8cf42df455d56e756e0e1679707230aa70b9bc03
unknown <pk1550756@gmail.com>
2023-02-20 10:41:33 +0530

    location Api's

--- controllers/season.py
+++ controllers/season.py
@@ -7,27 +7,89 @@ from models.season import Seasons
 def getseason(lan):
      try:     
         result = Seasons.getseasonAlldata(lan)
-        if result:
+        if result != False:
+            print(result)
             return ({'result':True,'data':result}), 200
         else:
             return invalid('Bad request',400)
      except Exception as e:
           return str(e)
 
+def get_state(lan):
+     try:     
+        result = Seasons.getstate(lan)
+        if result != False:
+            print(result)
+            return ({'result':True,'data':result}), 200
+        else:
+            return invalid('Bad request',400)
+     except Exception as e:
+          return str(e)
 
+def get_district(lan,uuid_state):
+     try:     
+        result = Seasons.getdistrict(lan,uuid_state)
+        if result != False:
+            print(result)
+            return ({'result':True,'data':result}), 200
+        else:
+            return invalid('Bad request',400)
+     except Exception as e:
+          return str(e)
+        
+        
+def get_tehsil(lan,uuid_district):
+     try:     
+        result = Seasons.gettehsil(lan,uuid_district)
+        if result != False:
+            print(result)
+            return ({'result':True,'data':result}), 200
+        else:
+            return invalid('Bad request',400)
+     except Exception as e:
+          return str(e)        
+        
 
-def bookmark_add_remove(uuid,fav):
-    active = "added successfully"
-    deactive =" bookmark removed successfully"
+# def bookmark_add_remove(uuid,fav):
+#     active = "added successfully"
+#     deactive =" bookmark removed successfully"
     
-    try:    
-        if uuid and fav == 1:
-            result = Seasons.bookmark_add(uuid,fav)
-            return ({'result':True,'message':active}),202
+#     try:    
+#         if uuid and fav == 1:
+#             result = Seasons.bookmark_add(uuid,fav)
+#             return ({'result':True,'message':active}),202
     
                 
+#         else:
+#             result = Seasons.remove_bookMark(uuid,fav)
+#             return ({'result':True,'message':deactive}),202
+#     except Exception as e:
+#           return str(e)
+
+def user_create(**user):
+    try:   
+        if user['uuid_tehsil']:
+            
+          result = Seasons.User(**user)
+          msg = " successfully created  User!"
+          return ({'result':True,"message": msg}), 200  
+      
         else:
-            result = Seasons.remove_bookMark(uuid,fav)
-            return ({'result':True,'message':deactive}),202
+             return invalid('Bad request',400)
+        
+    except Exception as e:
+          return str(e)
+    
+def user_device(**user_device):
+    try:   
+        if user_device['uuid_user']:
+            
+          result = Seasons.Userdevice(**user_device)
+          msg = " successfully created !"
+          return ({'result':True,"message": msg}), 200  
+      
+        else:
+             return invalid('Bad request',400)
+        
     except Exception as e:
           return str(e)
--- routes/seasons_bp.py
+++ routes/seasons_bp.py
@@ -9,22 +9,76 @@ api = Api(season__bp)
 
 
 class Seasons(Resource):
+    def post(self):
+        
+        if ( request.method == "POST"  ):
+            
+            name = request.form["name"]
+            phonenumber = request.form["phonenumber"]
+            email = request.form["email"]
+            image = request.form["image"]
+            address1 = request.form["address1"]
+            address2 = request.form["address2"]
+            uuid_tehsil = request.form["uuid_tehsil"]
+            geo_lat = request.form["geo_lat"]
+            geo_lng = request.form["geo_lng"]
+         
+            response = user_create(name = name,phonenumber = phonenumber,email =email,image =image,address1 =address1,address2=address2,uuid_tehsil=uuid_tehsil,geo_lat=geo_lat,geo_lng=geo_lng)
+            return response
+        
+        else:
+             return({'message':'bad request'}),400
      
-      def get(self,lan):
+    def get(self,lan):
           
         if (request.method == "GET" ):
             
             response = getseason(lan)
             return response
 
-class bookmark(Resource):
-     def get(self,uuid,fav):
+# class bookmark(Resource):
+#      def get(self,uuid,fav):
       
-        if uuid and fav == 1:
-            response = bookmarkAdd(uuid,fav)
-            return response
-        else:
-            response = bookmarkremove(uuid,fav)
-            return response    
+#         if uuid and fav == 1:
+#             response = bookmarkAdd(uuid,fav)
+#             return response
+#         else:
+#             response = bookmarkremove(uuid,fav)
+#             return response    
         
-api.add_resource(Seasons,"/seasonlist/<lan>")
+        
+class Location(Resource):
+      def get(self,lan,uuid_state = None,uuid_district = None):
+          
+          if uuid_state is not None and lan :
+              response = get_district(lan,uuid_state)
+              return response
+          
+          elif uuid_district is not None  and lan:
+              response = get_tehsil(lan,uuid_district)
+              return response
+          else:
+            response = get_state(lan)
+            return response
+
+class UserDevice(Resource):
+     def post(self):
+        
+        if ( request.method == "POST"  ):
+            uuid_user = request.form["uuid_user"]
+            device_id = request.form["device_id"]
+            device_name = request.form["device_name"]
+            device_model = request.form["device_model"]
+            platform_type = request.form["platform_type"]
+            os_version = request.form["os_version"]
+            fcm_token = request.form["fcm_token"]
+           
+            response = user_device(uuid_user = uuid_user,device_id = device_id,device_name =device_name,device_model =device_model,platform_type =platform_type,os_version=os_version,fcm_token=fcm_token)
+            return response
+        
+        else:
+             return({'message':'bad request'}),400
+    
+    
+api.add_resource(Seasons,"/seasonlist/<lan>","/user")
+api.add_resource(Location,"/state/<lan>","/district/<uuid_state>/<lan>","/tehsil/<uuid_district>/<lan>")
